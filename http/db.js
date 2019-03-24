65e3be6bf14ee47473f7070a1bd93fcb77ac99df
Leo Nesfield <leo@thelmgn.com>
2019-03-24 19:28:40 +0000

    serve files

--- http/db.js
+++ http/db.js
@@ -1,3 +1,20 @@
+const path = require("path")
 module.exports = function(app) {
-
+    app.use("/:id", async function(req,res,next) {
+        var id = req.params.id
+        var db = global.db.get({id: id})
+        if (db) {
+            if (db.type == "file") {
+                res.sendFile(path.resolve("./files/" + db.file))
+            } else if (db.type == "link") {
+                await res.set({
+                    Location: db.redir
+                });
+                await res.status(301);
+                res.end();        
+            }
+        } else {
+            next()
+        }
+    })
 }
