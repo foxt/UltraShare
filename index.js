d13c7197ee67228a3ea7f6045684054326680151
Leo Nesfield <leo@thelmgn.com>
2019-03-24 18:48:03 +0000

    TO TAKE ME HOME AT THE END OF THE NIGHT

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const Express = require('express')
 const db = global.db = require("./db");
 const config = require("./config")
@@ -7,12 +8,56 @@ const ejs = require('ejs')
 const app = Express()
 
 require("./http/api")(app)
-require("./http/db")(app)
+
+app.get("/", function(req,res) {
+    ejs.renderFile("./http/dynamic/hero.ejs", {
+        pageTitle: "UltraShare",
+        heroType: "primary is-bold",
+        heroTitle: "Welcome to UltraShare!",
+        heroText: "UltraShare is a all in one server for screenshots, files, images, and links.",
+        heroLinks: [
+                {
+                    color: "link is-large",
+                    icon: "account_circle",
+                    text: "Login",
+                    link: "/login.html"
+                }
+        ]
+    }, {}, function(err, str){
+        if (err) { throw err }
+        res.send(str)
+    })
+})
 
 app.use(Express.static('http/staticFiles'))
 
+require("./http/db")(app)
+
 app.get("/*", function(req,res) {
-    res.sendFile(path.resolve("./http/staticFiles/404.html"))
+    ejs.renderFile("./http/dynamic/hero.ejs", {
+        pageTitle: "404 - Not found",
+        heroType: "danger is-bold",
+        heroTitle: "<kbd>404</kbd>",
+        heroText: `The file at the URL <kbd>${req.url}</kbd> could not be found.`,
+        heroLinks: [
+            {
+                color: "link is-large",
+                icon: "arrow_back",
+                text: "",
+                link: "javascript:window.history.back()"
+            },
+            {
+                color: "link is-large",
+                icon: "home",
+                text: "",
+                link: "/"
+            }
+                
+        ]
+    }, {}, function(err, str){
+        if (err) { throw err }
+        res.send(str)
+    })
 }) 
 
 app.listen(config.port)
